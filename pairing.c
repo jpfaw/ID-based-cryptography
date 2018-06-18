3f613ce6b4c34bdf58eed3b656e49d1935a50820
jpfaw <isidoyuuta@gmail.com>
2018-06-18 18:52:19 +0900

    XOR演算の文字数関連の処理を実装

--- pairing.c
+++ pairing.c
@@ -6,16 +6,17 @@
 
 #include <gmp.h>
 #include <tepla/ec.h>
-#include<openssl/sha.h>
+#include <openssl/sha.h>
 
 typedef struct{
     EC_POINT P; // 楕円曲線上の点P
     EC_POINT Q; // 公開鍵
 } Public_key;
 
-void char_to_hash(unsigned char *hash, char* id);
-void print_unsigned_char(unsigned char *uc, char *dataName, size_t size);
-void exclusive_disjunction(unsigned char *ciphertext, unsigned char *hash, char *plaintext);
+void char_to_hash(unsigned char *hash, const char* id);
+void print_unsigned_char(const unsigned char *uc, const char *dataName, const size_t size);
+void exclusive_disjunction(unsigned char *ciphertext, const unsigned char *hash,
+                           const char *text, const size_t hashSize, const size_t textSize);
 
 // IDを貰ってE上の点Pへのハッシュ変換を行う(引数: 点P, ID)
 void hash1(EC_POINT P,const char *id) {
@@ -96,7 +97,7 @@ int main(void) {
 /* ----- Encode ----- */
     /* --- IDと平文m --- */
     char id[] = "shirase";
-    char m[]  = "hello_world!";
+    char m[]  = "hello_world!日本国民は、正当に選挙された国会における代表者を通じて行動し、われらとわれらの子孫のために、諸国民との協和による成果と、わが国全土にわたつて自由のもたらす恵沢を確保し、政府の行為によつて再び戦争の惨禍が起ることのないやうにすることを決意し、ここに主権が国民に存することを宣言し、この憲法を確定する。そもそも国政は、国民の厳粛な信託によるものであつて、その権威は国民に由来し、その権力は国民の代表者がこれを行使し、その福利は国民がこれを享受する。これは人類普遍の原理であり、この憲法は、かかる原理に基くものである。われらは、これに反する一切の憲法、法令及び詔勅を排除する。";
 
     /* --- AさんのIDをP_Aに変換 --- */
     EC_POINT P_A;
@@ -118,7 +119,7 @@ int main(void) {
     printf("rP=  ");
     point_print(rP);
 
-    /* --- e(P_A, sP)^r の計算 --- */
+    /* --- g = e(P_A, sP)^r の計算 --- */
     Element g;
     element_init(g, p->g3);
     pairing_map(g, sP, P_A, p); // 第3引数はg2を参照のこと
@@ -137,19 +138,21 @@ int main(void) {
         element_get_str(element_str, g);
         printf("element_str: %s\n", element_str);
     }
-    
-    /* --- 文字列のハッシュ化 --- */
-    unsigned char hash[SHA256_DIGEST_LENGTH];
-    char_to_hash(hash, element_str);
+
+    /* --- gのハッシュ化 --- */
+    unsigned char g_hash[SHA256_DIGEST_LENGTH];
+    char_to_hash(g_hash, element_str);
+
+    /* --- g_hashとmをXOR --- */
     unsigned char ciphertext[strlen(m)+1];
+    exclusive_disjunction(ciphertext, g_hash, m, strlen(g_hash), strlen(m));
+
+// 正常にXORできてるか検証
     unsigned char ciphertext2[strlen(m)+1];
-    
-    exclusive_disjunction(ciphertext, hash, m);
-    exclusive_disjunction(ciphertext2, hash, ciphertext); // 正常にXORできてるか検証
+    exclusive_disjunction(ciphertext2, g_hash, ciphertext, strlen(g_hash), strlen(m));
     printf("%s\n", ciphertext2);
 
-    //TODO: gとmをxor
-
+// この段階で、Enc(m)→暗号文C=(U, v)=(rP, ciphertext)
 
 /* ----- Decode ----- */
 
@@ -174,11 +177,10 @@ int main(void) {
  * $0 ハッシュ化したものを入れるchar配列ポインタ
  * $1 ハッシュ化する文字列
   -----------------------------------------*/
-void char_to_hash(unsigned char *hash, char* id){
+void char_to_hash(unsigned char *hash, const char* id){
     
     //    unsigned char hash[SHA256_DIGEST_LENGTH];
     SHA256(id,strlen(id),hash);
-    
     /* --- debug print --- */
     print_unsigned_char(hash, "hash", SHA256_DIGEST_LENGTH);
 }
@@ -187,25 +189,22 @@ void char_to_hash(unsigned char *hash, char* id){
  * 排他的論理和を計算する関数
  * $0 計算結果を入れるu_char配列ポインタ
  * $1 XORする値
- * $2 平文
- * 今は平文の方が短い場合のみ対応
+ * $2 平文/暗号化文
+ * $3 ハッシュの文字数
+ * $4 テキストの文字数
  -----------------------------------------*/
-void exclusive_disjunction(unsigned char *ciphertext, unsigned char *hash, char *plaintext) {
-
-    size_t index = strlen(plaintext);
-
-    // 文字列の長さを合わせる
-    if(strlen(hash) >= strlen(plaintext)) {
-        hash[index] = '\0';
-    }
-
+void exclusive_disjunction(unsigned char *ciphertext, const unsigned char *hash,
+                           const char *text, const size_t hashSize, const size_t textSize) {
+    printf("hashSize: %d\ntextSize: %d\n", strlen(text), textSize);
+    
+    //TODO: hashの文字数がXORした後に変動する
     // 1文字ずつ分解し、XOR演算する
-    for(size_t i=0; i<strlen(plaintext); i++){
-        ciphertext[i] = plaintext[i]^hash[i];
+    for(size_t i=0; i<textSize; i++){
+        ciphertext[i] = text[i]^hash[i%hashSize];
     }
 
-    ciphertext[index] = '\0';
-    print_unsigned_char(ciphertext, "ciphertext", index);
+    ciphertext[textSize] = '\0';
+    print_unsigned_char(ciphertext, "ciphertext", textSize);
 }
 
 /* -----------------------------------------------
@@ -215,7 +214,7 @@ void exclusive_disjunction(unsigned char *ciphertext, unsigned char *hash, char 
  * $2 データサイズ
  * 今は平文の方が短い場合のみ対応
  -----------------------------------------------*/
-void print_unsigned_char(unsigned char *uc, char *dataName, size_t size){
+void print_unsigned_char(const unsigned char *uc, const char *dataName, const size_t size){
     printf("%s: ", dataName);
     for (size_t i=0; i<size; i++){
         printf("%02x", uc[i] );
